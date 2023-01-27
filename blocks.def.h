b9f58a5b23be70bd1d91dc20427ebcd48a423a05
os-av <os.av@tutanota.com>
2023-01-27 06:43:27 +0000

    brightness block

--- blocks.def.h
+++ blocks.def.h
@@ -3,9 +3,10 @@
 static const Block blocks[] = {
 	  /*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
     {"",          "volume",											  0,      10},
+	{"",          "brightness",										  0,      10},
     {"",          "battery",									     60,       0},
-	{"",        "cpu",                                                5,       0},
-	{"",        "mem",                                                5,       0},
+	{"",          "mem",                                              5,       0},
+	{"",          "cpu",                                              5,       0},
 	{" ",        "date '+%H:%M'",                                   60,       0},
 	{" ",        "date '+%a, %x'",						             60,	   0},
 };
